8950461228ca83cdef01a84ba294b442e51fc731
Harsh Rathi <rathiharsh2002@gmail.com>
2022-06-08 23:13:00 +0530

    add: modelling for grreater functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,134 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen};
+use near_sdk::collections::{LookupMap, UnorderedSet};
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, PanicOnDefault};
 
 const PUZZLE_NUMBER: u8 = 1;
 
+#[derive(BorshDeserialize, BorshSerialize, Debug, Serialize,Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+enum PuzzleStatus {
+    Unsolved,
+    Solved {memo:String},  //eg "Took me forever to get clue six!" (winners can write this) 
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub enum AnswerDirection {
+    Across,
+    Down,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct CoordinatePair {
+    x: u8,
+    y: u8,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Answer {
+    num: u8,
+    start: CoordinatePair,      //struct
+    direction: AnswerDirection, //enum
+    length: u8,
+    clue: String,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+pub struct Puzzle {
+    status: PuzzleStatus,
+    answer: Vec<Answer>,
+}
+
+#[derive(Serialize,Deserialize)]  //json serializing
+#[serde(crate = "near_sdk::serde")]
+pub struct JsonPuzzle {
+    //for returning data to frontend 
+    solution_hash:String,
+    status : PuzzleStatus,
+    answer : Vec<Answer>,
+
+}
+
+// Remember that there will be only one struct that gets the #[near_bindgen] macro placed on it; our primary struct or singleton
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
-    crossword_solution: String,
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Crossword {
+    // SETUP Crossword STATE
+    owner_id: AccountId,
+    puzzles: LookupMap<String, Puzzle>, //non iterable,solution_hash and its puzzle
+    unsolved_puzzles: UnorderedSet<String>, //like an interable list containing the solution hash
+                                        // because cant iterate through puzzles to find unsolved ones (costs a lot of gas)
 }
 
 #[near_bindgen]
-impl Contract {
-    #[init] //initialization macro for the contract
-    pub fn new(solution: String) -> Contract {
-        Contract {
-            crossword_solution: solution,
+impl Crossword {
+    #[init] //initialization macro for the Crossword
+    pub fn new(owner_id: AccountId) -> Crossword {
+        // When a contract gets complicated, there may be multiple different collections that are not all part of the main structure, but instead part of a sub-structure or nested collections. They all need to have unique prefixes.
+        Crossword {
+            owner_id,
+            puzzles: LookupMap::new(b"c"), //unique prefix, best practice is to make a enum of it`
+            unsolved_puzzles: UnorderedSet::new(b"u"),
         }
     }
-    // ADD CONTRACT METHODS HERE
+    // ADD Crossword METHODS HERE
     pub fn get_puzzle_number(&self) -> u8 {
         PUZZLE_NUMBER
     }
 
-    // changes state
-    pub fn set_solution(&mut self, solution: String) {
-        self.crossword_solution = solution;
+    // for the owner to insert puzzles
+    pub fn new_puzzle(&mut self, solution_hash: String, answers: Vec<Answer>) {
+        // even a person with full access keys cannot call this method due to this check
+        assert_eq!(
+            env::predecessor_account_id(), //predecessor:- the person who most recently called this method,
+            self.owner_id,
+            "Only the owner of the contract may call this method"
+        );
+
+        // add the new unsolved puzzle in the existing list of puzzles
+
+        // Inserts a key-value pair into the lookup map. If the map did not have this key present, None is returned.
+        let existing = self.puzzles.insert(
+            &solution_hash,
+            &Puzzle {
+                status: PuzzleStatus::Unsolved,
+                answer: answers,
+            },
+        );
+
+        // check for the key
+        assert!(existing.is_none(), "Puzzle with that key already exists");
+
+        // add the puzzle in the specefic unsolved list
+        self.unsolved_puzzles.insert(&solution_hash);
     }
+
+    // changes state
+    // pub fn set_solution(&mut self, solution: String) {
+    //     self.crossword_solution = solution;
+    // }
 
     //creates a transaction id while executing the script
-    pub fn guess_solution(&mut self, solution: String) -> bool {
-        //solution is the user guess
+    // pub fn guess_solution(&mut self, solution: String) -> bool {
+    //     //solution is the user guess
 
-        // convert the user solution to sha256 and then compare it
-        let hashed_input = env::sha256(solution.as_bytes());
-        let hashed_input_hex = hex::encode(&hashed_input);
+    //     // convert the user solution to sha256 and then compare it
+    //     let hashed_input = env::sha256(solution.as_bytes());
+    //     let hashed_input_hex = hex::encode(&hashed_input);
 
-        if hashed_input_hex == self.crossword_solution {
-            env::log_str("You guessed right");
-            true
-        } else {
-            env::log_str("Try again for winning the crossword puzzle");
-            false
-        }
-        // Well, logging is ultimately captured inside blocks added to the blockchain. (More accurately, transactions are contained in chunks and chunks are contained in blocks. More info in the Nomicon spec.) So while it is not changing the data in the fields of the struct, it does cost some amount of gas to log, requiring a signed transaction by an account that pays for this gas.
-    }
+    //     if hashed_input_hex == self.crossword_solution {
+    //         env::log_str("You guessed right");
+    //         true
+    //     } else {
+    //         env::log_str("Try again for winning the crossword puzzle");
+    //         false
+    //     }
+    //     // Well, logging is ultimately captured inside blocks added to the blockchain. (More accurately, transactions are contained in chunks and chunks are contained in blocks. More info in the Nomicon spec.) So while it is not changing the data in the fields of the struct, it does cost some amount of gas to log, requiring a signed transaction by an account that pays for this gas.
+    // }
 }
-
 
 // use the attribute below for unit tests
 #[cfg(test)]
@@ -55,7 +137,7 @@ mod tests {
     use near_sdk::test_utils::{get_logs, VMContextBuilder};
     use near_sdk::{testing_env, AccountId};
 
-    //The important thing to remember is VMContext will be sending mock transactions with the context specified above. So if a unit test needs to send a test transaction coming from Alice, and then another from Bob, the get_context method may be called to change the signer_account_id or predecessor_account_id, or whatever the contract needs.
+    //The important thing to remember is VMContext will be sending mock transactions with the context specified above. So if a unit test needs to send a test transaction coming from Alice, and then another from Bob, the get_context method may be called to change the signer_account_id or predecessor_account_id, or whatever the Crossword needs.
 
     // here the predecessor id is in our control for the tests
     fn get_context(predecessor: AccountId) -> VMContextBuilder {
@@ -80,20 +162,20 @@ mod tests {
         //"69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f"
     }
 
-    #[test]
-    fn check_guess_solution() {
-        // get harsh's account id
-        let harsh = AccountId::new_unchecked("harshrathi2511.testnet".to_string());
-        //set the context and the test environment
-        let context = get_context(harsh);
-        testing_env!(context.build());
+    // #[test]
+    // fn check_guess_solution() {
+    //     // get harsh's account id
+    //     let harsh = AccountId::new_unchecked("harshrathi2511.testnet".to_string());
+    //     //set the context and the test environment
+    //     let context = get_context(harsh);
+    //     testing_env!(context.build());
 
-        //create the contract with the correct solution
-        let mut contract = Contract::new(
-            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
-        );
+    //     //create the Crossword with the correct solution
+    //     let mut Crossword = Crossword::new(
+    //         "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+    //     );
 
-        let guess_result = contract.guess_solution("near nomicon ref finance".to_string());
-        assert!(guess_result, "this will pass for this");
-    }
+    //     let guess_result = Crossword.guess_solution("near nomicon ref finance".to_string());
+    //     assert!(guess_result, "this will pass for this");
+    // }
 }
